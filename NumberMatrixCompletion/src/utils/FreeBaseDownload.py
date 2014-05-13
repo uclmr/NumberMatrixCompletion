9c41a54cc084c982503b9bad7ad7bb50c45b2d94
Andreas Vlachos <av308@cl.cam.ac.uk>
2014-05-13 16:28:34 +0100

    Adding things to github

--- NumberMatrixCompletion/src/utils/FreeBaseDownload.py
+++ NumberMatrixCompletion/src/utils/FreeBaseDownload.py
@@ -6,10 +6,12 @@ Created on May 10, 2014
 import json
 import urllib
 
+
 api_key = "AIzaSyClJFx89pJR0_8yc1nvTClMUzFPj0r1dHA"
 mqlread_url = 'https://www.googleapis.com/freebase/v1/mqlread'
 # use the mid instead of the id as they do need escaping
 mql_query = '[{"mid": null,"name": null, "type": "/location/statistical_region","limit": 100}]'
+# set this to the last value we obtained
 cursor = "eNpVjEEKwkAQBL-jyJKeyc7uzCDiP0IOcWUhICYkxKNvV3IQ7GMV1WVb1mlxw616NzmrRaDvnk7QqCTE0o_r4Ag7QE5k_jiHw2uc3UyFkuSfY_hyMgYPWvA9QopUFX8r4bJ3MbbHhkTabAL2d-jW2dkSEvXNNakpRdGKfK8FliPSB9DJKhs="
 
 api_key = "AIzaSyClJFx89pJR0_8yc1nvTClMUzFPj0r1dHA"
@@ -29,11 +31,11 @@ for i in xrange(1000):
     statisticalRegionsResult = json.loads(urllib.urlopen(mql_url).read())
     print statisticalRegionsResult
     for region in statisticalRegionsResult["result"]:
-        print region["mid"]# + ":" + region["name"]
+        print region["mid"]  # + ":" + region["name"]
         # now get the statistical properties
-        topic_url = topicService_url +  region["mid"] + '?' + urllib.urlencode(params)
+        topic_url = topicService_url + region["mid"] + '?' + urllib.urlencode(params)
         topicResult = json.loads(urllib.urlopen(topic_url).read())
-        #print topicResult
+        # print topicResult
         topicResult["name"] = region["name"]
         filename = region["mid"].split("/")[-1]
         with open(filename, 'w') as outfile:
@@ -41,7 +43,7 @@ for i in xrange(1000):
 
     # update the cursor
     cursor = statisticalRegionsResult['cursor']
-    # this gues can be used to resume the data download
+    # this cursor can be used to resume the data download
     print "New cursor to process"
     print cursor
     if not cursor:
--- NumberMatrixCompletion/src/utils/numberExtraction.py
+++ NumberMatrixCompletion/src/utils/numberExtraction.py
@@ -4,7 +4,46 @@ Created on May 10, 2014
 @author: andreasvlachos
 '''
 
-import json.
+import json
+import os
+
+# given a json extract the most recent numerical value for each of the properties mentioned
+def extractNumericalValues(jsonObj):
+    country = json.loads(jsonObj)
+    name = country["name"]
+    print name
+    # for each property
+    for prop, value in country["property"].items():
+        # get the name 
+        print "Property=" + prop
+        # keep the valuetype for this property
+        #valueType = value["valuetype"]
+        #print valueType
+        mostRecentValue = 0
+        mostRecentTime = 0
+        for val in value["values"]:
+            #print val["property"].keys()
+            thisValue = None
+            thisTime = None
+            for item, v in val["property"].items():
+                #print item
+                if item.split("/")[-1] in ["amount", "rate", "number", "adjusted_value", "cubic_meters", "index_value", "days"]:
+                    thisValue = v["values"][0]["value"]
+                elif item.split("/")[-1] in ["date", "valid_date", "year", "measurement_date"]:
+                    thisTime = v["values"][0]["value"]
+            if thisTime > mostRecentTime:
+                mostRecentTime = thisTime
+                mostRecentValue = thisValue
+        # in some cases we do not get any suitable values, r.g. religion
+        if mostRecentTime >0:
+            print "Time=" + str(mostRecentTime) + " Value=" + str(mostRecentValue)
+                
+                
+        #TODO: what about things that have months and years, e.g. major_exports, minimum wage    
+    
 
 if __name__ == '__main__':
-    pass
+    dirName = "../../../FreeBaseData"
+    for fl in os.listdir(dirName):
+        jsonFl = open(dirName + "/" + fl).read()
+        extractNumericalValues(jsonFl)

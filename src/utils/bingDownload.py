a90ae573220d71f09741fc4d7f30eafca6415c2e
andreasvlachos <av308@cl.cam.ac.uk>
2014-06-06 19:27:19 +0100

    updated the bing downloader

--- src/utils/bingDownload.py
+++ src/utils/bingDownload.py
@@ -3,21 +3,59 @@
 
 import urllib2
 import base64
+import urllib
+import json
+import os
 
-# TODO: these are to be loaded from the jsons
+
+
+# from here we only want to keep the countries
+with open("/cs/research/intelsys/home1/avlachos/FactChecking/allCountriesPost2010Filtered15-150.json") as dataFile:
+    allCountriesData = json.loads(dataFile.read())
+
+
+with open("/cs/research/intelsys/home1/avlachos/FactChecking/featuresKept.json") as dataFile:
+    featuresKept = json.loads(dataFile.read())
+
 # better lowercase the property names
+with open("/cs/research/intelsys/home1/avlachos/FactChecking/allStatisticalRegionProperties.json") as dataFile:
+    featuresDesc = json.loads(dataFile.read())
+
+
+#TODO: try this out with a few first, say 3 countries and 3 features to make sure that the pipeline works end to end
 propertyKeywords = ["population"]
 countryNames = ["Czech Republic"]
 
-bingUrl = 'https://api.datamarket.azure.com/Bing/Search/Web' # ?Query=%27gates%27&$top=10&$format=json'
+
+
+bingUrl = 'https://api.datamarket.azure.com/Bing/SearchWeb/v1/Web' # ?Query=%27gates%27&$top=10&$format=json'
 #Provide your account key here
 accountKey = 'ZAk6G5VxGSD+K/mx3QH+PX24x85Cx9lEVnQzXA5H+P0'
 accountKeyEnc = base64.b64encode(accountKey + ':' + accountKey)
 headers = {'Authorization': 'Basic ' + accountKeyEnc}
 
+pathName = "/cs/research/intelsys/home1/avlachos/FactChecking/Bing"
 
-req = urllib2.Request(bingUrl, headers = headers)
-response = urllib2.urlopen(req)
-content = response.read()
-#content contains the xml/json response from Bing. 
-print content
+if os.path.exists(pathName):
+    os.mkdir(pathName)
+
+for name in countryNames:
+    for keywords in propertyKeywords:
+        params = {
+                 #'format': "Json",
+                  'Adult': "\'Strict\'",
+                  'WebFileType' : "\'HTML\'"
+                  }
+        # the query terms are done with urllib quote in order to get %20 instead of + (bing likes that instead)
+        url = bingUrl + "?Query=" + urllib.quote("\'" + name + " " + keywords + "\'") + "&" + urllib.urlencode(params) + "&$format=json"
+        print url
+        req = urllib2.Request(url, headers = headers)
+        response = urllib2.urlopen(req)
+        content = json.loads(response.read())
+        #content contains the xml/json response from Bing. 
+        print content
+        filename = pathName + 
+        with open(filename, 'w') as outfile:
+            json.dump(content, outfile)
+        
+        # save the json in a file named after the relation and the property.
--- src/utils/dataFiltering.py
+++ src/utils/dataFiltering.py
@@ -14,7 +14,7 @@ print len(data)
 filteredFeatureCounts = Counter()
 filteredCountries = {}
 for country, numbers in data.items():
-    if len(numbers) >= 2:
+    if len(numbers) >= 15:
         filteredCountries[country] = numbers
         for feature in numbers:
             filteredFeatureCounts[feature] += 1
@@ -23,17 +23,28 @@ for country, numbers in data.items():
 print filteredFeatureCounts 
 
 filteredFeatureCountries = {}
+featuresKept = []
+entriesFilled = 0
 for country, numbers in filteredCountries.items():
     filteredFeatures = {}
     for feature, number in numbers.items():
-        if filteredFeatureCounts[feature] >= 10:
+        if filteredFeatureCounts[feature] >= 150:
+            if feature not in featuresKept:
+                featuresKept.append(feature)
             filteredFeatures[feature] = number
     filteredFeatureCountries[country] = filteredFeatures
+    entriesFilled += len(filteredFeatures)
+
 
 print len(filteredFeatureCountries)
+print len(featuresKept)
+print entriesFilled
 
-with open("/cs/research/intelsys/home1/avlachos/FactChecking/allCountriesPost2010Filtered.json", "w") as dataFile:
+with open("/cs/research/intelsys/home1/avlachos/FactChecking/allCountriesPost2010Filtered15-150.json", "w") as dataFile:
     json.dump(filteredFeatureCountries, dataFile)
+
+with open("/cs/research/intelsys/home1/avlachos/FactChecking/featuresKept.json", "w") as dataFile:
+    json.dump(featuresKept, dataFile)
 
 
 #print len(featureCounts)

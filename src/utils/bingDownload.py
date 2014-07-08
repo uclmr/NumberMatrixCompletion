b265497046607383fda61c7d0956cb6351e5ee24
andreasvlachos <av308@cl.cam.ac.uk>
2014-07-08 18:24:11 +0100

    Finished Bing downloader

--- src/utils/bingDownload.py
+++ src/utils/bingDownload.py
@@ -7,13 +7,11 @@ import urllib
 import json
 import os
 
-
-
 # from here we only want to keep the countries
 with open("/cs/research/intelsys/home1/avlachos/FactChecking/allCountriesPost2010Filtered15-150.json") as dataFile:
     allCountriesData = json.loads(dataFile.read())
 
-
+# from here get the property ids
 with open("/cs/research/intelsys/home1/avlachos/FactChecking/featuresKept.json") as dataFile:
     featuresKept = json.loads(dataFile.read())
 
@@ -22,11 +20,21 @@ with open("/cs/research/intelsys/home1/avlachos/FactChecking/allStatisticalRegio
     featuresDesc = json.loads(dataFile.read())
 
 
-#TODO: try this out with a few first, say 3 countries and 3 features to make sure that the pipeline works end to end
-propertyKeywords = ["population"]
-countryNames = ["Czech Republic"]
 
+propertyKeywords = []
+for feature in featuresKept:
+    # get the name for it and lower case it
+    for feat in featuresDesc["result"]:
+        if feat["id"] == feature:
+            propertyKeywords.append(feat["name"].lower().encode('utf-8'))
+#print propertyKeywords
+#propertyKeywords = ["population"]
 
+countryNames = []
+for country in allCountriesData:
+    countryNames.append(country.encode('utf-8'))
+#print countryNames
+#countryNames = ["Czech Republic"]
 
 bingUrl = 'https://api.datamarket.azure.com/Bing/SearchWeb/v1/Web' # ?Query=%27gates%27&$top=10&$format=json'
 #Provide your account key here
@@ -36,26 +44,31 @@ headers = {'Authorization': 'Basic ' + accountKeyEnc}
 
 pathName = "/cs/research/intelsys/home1/avlachos/FactChecking/Bing"
 
-if os.path.exists(pathName):
+if not os.path.exists(pathName):
+    print "creating dir"
     os.mkdir(pathName)
 
 for name in countryNames:
+    print name
     for keywords in propertyKeywords:
+        print keywords
         params = {
                  #'format': "Json",
                   'Adult': "\'Strict\'",
                   'WebFileType' : "\'HTML\'"
                   }
         # the query terms are done with urllib quote in order to get %20 instead of + (bing likes that instead)
-        url = bingUrl + "?Query=" + urllib.quote("\'" + name + " " + keywords + "\'") + "&" + urllib.urlencode(params) + "&$format=json"
+        # Note that add the current year (2014) since Bing API doesn't allow us search only recent documents
+        #print '\''.encode('utf-8') + name + " " + keywords + u' 2014\''.encode('utf-8')
+        url = bingUrl + "?Query=" + urllib.quote('\''.encode('utf-8') + name + " " + keywords + ' 2014\''.encode('utf-8')) + "&" + urllib.urlencode(params) + "&$format=json"
         print url
         req = urllib2.Request(url, headers = headers)
         response = urllib2.urlopen(req)
         content = json.loads(response.read())
-        #content contains the xml/json response from Bing. 
+        # content contains the xml/json response from Bing. 
         print content
-        filename = pathName + 
+        # save the json in a file named after the country and the property.        
+        filename = pathName + "/" + name + "_" + keywords + ".json"
         with open(filename, 'w') as outfile:
-            json.dump(content, outfile)
+            json.dump(content["d"]["results"], outfile)
         
-        # save the json in a file named after the relation and the property.

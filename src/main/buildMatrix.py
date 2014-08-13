e415eba986c663db48694610a1be30fe1917c31a
Andreas Vlachos <av308@cl.cam.ac.uk>
2014-08-13 18:47:24 +0100

    Finished implementing the first version.

--- src/main/buildMatrix.py
+++ src/main/buildMatrix.py
@@ -16,11 +16,11 @@ import networkx
 
 # this class def allows us to write:
 #print(json.dumps(np.arange(5), cls=NumPyArangeEncoder))
-class NumPyArangeEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, numpy.ndarray):
-            return obj.tolist() # or map(int, obj)
-        return json.JSONEncoder.default(self, obj)
+#class NumPyArangeEncoder(json.JSONEncoder):
+#    def default(self, obj):
+#        if isinstance(obj, numpy.ndarray):
+#            return obj.tolist() # or map(int, obj)
+#        return json.JSONEncoder.default(self, obj)
 
 
 parsedJSONDir = sys.argv[1]
@@ -33,6 +33,8 @@ outputFile = sys.argv[2]
 
 # This is the location2deps2values
 theMatrix = {}
+
+print str(len(jsonFiles)) + " files to process"
 
 for jsonFileName in jsonFiles:
     print "processing " + jsonFileName
@@ -63,8 +65,8 @@ for jsonFileName in jsonFiles:
                 except ValueError:
                     pass
                                 
-        print locationIDs
-        print numberIDs
+        #print locationIDs
+        #print numberIDs
         
         # if there was at least one location and one number build the dependency graph:
         if len(locationIDs) > 0 and len(numberIDs) > 0:
@@ -75,8 +77,8 @@ for jsonFileName in jsonFiles:
                 for idx in locationID:
                     locationTokens.append(sentence["tokens"][idx]["word"]) 
                 locations.append(" ".join(locationTokens))
-            print locations
-            print numbers
+            #print locations
+            #print numbers
             
             sentenceDAG = networkx.DiGraph()
             for idx, token in enumerate(sentence["tokens"]):
@@ -95,15 +97,43 @@ for jsonFileName in jsonFiles:
             # get the pairs of each and find their dependency paths (might be more than one) 
             for i, locationID in enumerate(locationIDs):
                 locationName = locations[i]
-                for numberID in numberIDs:
+                for j, numberID in enumerate(numberIDs):
+                    number = numbers[j]
                     # keep all the shortest paths between the number and the tokens of the location
-                    shorstesPaths = []
-                    for tokenID in locationsID:                  
-                        tempShortestPaths = networkx.all_shortest_paths(sentenceDAG, source=tokenID, target=numberID)
-                        if (len(shortestPaths) == 0) or len(shortestPaths[0]) > len(tempShortestPaths[0]):
-                            shortestPaths = tempShortestPaths
-                        elif  len(shortestPaths[0]) == len(tempShortestPaths[0]):
-                            shortestPaths.extend(tempShortestPaths)
-                
+                    shortestPaths = []
+                    for tokenID in locationID:
+                        try:
+                            # get the shortest paths
+                            # get the list as it they are unlikely to be very many and we need to len()                  
+                            tempShortestPaths = list(networkx.all_shortest_paths(sentenceDAG, source=tokenID, target=numberID))
+                            # if the paths found are shorter than the ones we had (or we didn't have any)
+                            if (len(shortestPaths) == 0) or len(shortestPaths[0]) > len(tempShortestPaths[0]):
+                                shortestPaths = tempShortestPaths
+                            # if they have equal length add them
+                            elif  len(shortestPaths[0]) == len(tempShortestPaths[0]):
+                                shortestPaths.extend(tempShortestPaths)
+                        # if not paths were found, do nothing
+                        except networkx.exception.NetworkXNoPath:
+                            pass
+                    for shortestPath in shortestPaths:
+                        # get the first dep
+                        pathStrings = []
+                        pathStrings.append(sentenceDAG[shortestPath[0]][shortestPath[1]]["label"])
+                        # for the words in between add the lemma and the dep
+                        for seq, tokenIDX in enumerate(shortestPath[1:-1]):
+                            pathStrings.append(sentence["tokens"][tokenIDX]["lemma"] + "~" + sentenceDAG[tokenIDX][shortestPath[seq+2]]["label"])
+                        pathString = "-".join(pathStrings)
+                        #print locationName + ":" + pathString  + ":" + str(number)
                         
-    
+                        if pathString not in theMatrix:
+                            theMatrix[pathString] = {}
+                            
+                        if locationName not in theMatrix[pathString]:
+                            theMatrix[pathString][locationName] = []
+                        
+                        theMatrix[pathString][locationName].append(number)
+#print theMatrix 
+                        
+with open(outputFile, "wb") as out:
+    json.dump(theMatrix, out)
+                        

a87744ba59a87711bbe8f51ef0f551d2bf4de70b
andreasvlachos <av308@cl.cam.ac.uk>
2014-09-03 19:38:36 +0100

    Each relation now has its own directory.

--- src/utils/bingDownload.py
+++ src/utils/bingDownload.py
@@ -27,14 +27,15 @@ for feature in featuresKept:
     for feat in featuresDesc["result"]:
         if feat["id"] == feature:
             propertyKeywords.append(feat["name"].lower().encode('utf-8'))
+#propertyKeywords = ["fertility rate"]
 #print propertyKeywords
-#propertyKeywords = ["population"]
 
 countryNames = []
 for country in allCountriesData:
     countryNames.append(country.encode('utf-8'))
-#print countryNames
+
 #countryNames = ["Czech Republic"]
+#print countryNames
 
 bingUrl = 'https://api.datamarket.azure.com/Bing/SearchWeb/v1/Web' # ?Query=%27gates%27&$top=10&$format=json'
 #Provide your account key here
@@ -42,28 +43,34 @@ accountKey = 'ZAk6G5VxGSD+K/mx3QH+PX24x85Cx9lEVnQzXA5H+P0'
 accountKeyEnc = base64.b64encode(accountKey + ':' + accountKey)
 headers = {'Authorization': 'Basic ' + accountKeyEnc}
 
-pathName = "/cs/research/intelsys/home1/avlachos/FactChecking/Bing"
+pathName = "/cs/research/intelsys/home1/avlachos/FactChecking/Bing2"
 
 if not os.path.exists(pathName):
-    print "creating dir"
+    print "creating dir " + pathName
     os.mkdir(pathName)
 
-for name in countryNames:
-    print name
-    for keywords in propertyKeywords:
-        print keywords
+
+for keywords in propertyKeywords:
+    print keywords
+    # create a directory for the relation
+    relPathName = pathName + "/" + keywords
+    if not os.path.exists(relPathName):
+        print "creating dir " +  relPathName
+        os.mkdir(relPathName)
+    
+    for name in countryNames:
+        print name
         params = {
                  #'format': "Json",
                   'Adult': "\'Strict\'",
-                  'WebFileType' : "\'HTML\'"
+                  'WebFileType' : "\'HTML\'",
                   }
         # the query terms are done with urllib quote in order to get %20 instead of + (bing likes that instead)
-        # Note that add the current year (2014) since Bing API doesn't allow us search only recent documents
         #print '\''.encode('utf-8') + name + " " + keywords + u' 2014\''.encode('utf-8')
-        # TODO: add in the end this bit
-        #&Adult=%27Strict%27&WebSearchOptions=%27DisableQueryAlterations%27
-        # TODO: rerun without 2014, it actually gives us world cup results...
-        url = bingUrl + "?Query=" + urllib.quote('\''.encode('utf-8') + name + " " + keywords + ' 2014\''.encode('utf-8')) + "&" + urllib.urlencode(params) + "&$format=json"
+        # one can add in the end this bit 'WebSearchOptions'  : "DisableQueryAlterations"
+        #&WebSearchOptions=%27DisableQueryAlterations%27
+        # this bit can fetch the second page "$skip=100"
+        url = bingUrl + "?Query=" + urllib.quote('\''.encode('utf-8') + name + " " + keywords + '\''.encode('utf-8')) + "&" + urllib.urlencode(params) + "&$format=json"
         print url
         req = urllib2.Request(url, headers = headers)
         response = urllib2.urlopen(req)
@@ -71,7 +78,7 @@ for name in countryNames:
         # content contains the xml/json response from Bing. 
         print content
         # save the json in a file named after the country and the property.        
-        filename = pathName + "/" + name + "_" + keywords + ".json"
+        filename = relPathName + "/" + name + ".json"
         with open(filename, 'w') as outfile:
             json.dump(content["d"]["results"], outfile)
         

9222c33acecc5b2e6f84d5354a494f6ee94eff49
andreasvlachos <av308@cl.cam.ac.uk>
2014-10-22 17:41:12 +0100

    Fixed bugs with nans and infs in the values, as well as some of the names that Stanford couldn't recognize.

--- src/main/buildMatrix.py
+++ src/main/buildMatrix.py
@@ -14,6 +14,8 @@ import glob
 import networkx
 import re
 import copy
+import numpy
+import codecs
 
 # this class def allows us to write:
 #print(json.dumps(np.arange(5), cls=NumPyArangeEncoder))
@@ -31,29 +33,50 @@ def getNumbers(sentence):
     for idx, token in enumerate(sentence["tokens"]):
         # avoid only tokens known to be dates or part of locations
         # This only takes actual numbers into account thus it ignores things like "one million"
-        # and also treating "500 millions" as "500" 
+        # and also treating "500 millions" as "500"
         if token["ner"] not in ["DATE", "LOCATION", "PERSON", "ORGANIZATION", "MISC"]:
             try:
                 # this makes sure that 123,123,123.23 which fails the float test, becomes 123123123.23 which is good
-                tokenWithoutCommas = float(re.sub(",([0-9][0-9][0-9])", "\g<1>", token["word"]))
+                tokenWithoutCommas = re.sub(",([0-9][0-9][0-9])", "\g<1>", token["word"])
                 number = float(tokenWithoutCommas)
-                ids = [idx]
-                # check the next token if it is million or thousand
-                if len(sentence["tokens"]) > idx+1:
-                    if sentence["tokens"][idx+1]["word"].startswith("million"):
-                        number = number * 1000000
-                        ids.append(idx+1)
-                        #print sentence["tokens"]
-                        #print number
-                    elif sentence["tokens"][idx+1]["word"].startswith("thousand"):
-                        number = number * 1000
-                        ids.append(idx+1)       
-                        #print sentence["tokens"]
-                        #print number             
-                tokenIDs2number[tuple(ids)] = number
+                # we want this to avoid taking in nan, inf and -inf as floats
+                if numpy.isfinite(number):
+                    ids = [idx]
+                    # check the next token if it is million or thousand
+                    if len(sentence["tokens"]) > idx+1:
+                        if sentence["tokens"][idx+1]["word"].startswith("million"):
+                            number = number * 1000000
+                            ids.append(idx+1)
+                            #print sentence["tokens"]
+                            #print number
+                        elif sentence["tokens"][idx+1]["word"].startswith("thousand"):
+                            number = number * 1000
+                            ids.append(idx+1)       
+                            #print sentence["tokens"]
+                            #print number
+
+                    tokenIDs2number[tuple(ids)] = number          
+                    
             except ValueError:
                 pass
     return tokenIDs2number
+
+# this function performs a dictNER matching to help with names that Stanford NER fails
+# use with caution, it ignores everything apart from the tokens, over-writing existing NER tags 
+def dictLocationMatching(sentence, tokenizedLocations):
+    # first re-construct the sentence as a string
+    wordsInSentence = [] 
+    for token in sentence["tokens"]:
+        wordsInSentence.append(token["word"])
+    #print wordsInSentence
+    for tokLoc in tokenizedLocations:
+        #print wordsInSentence
+        #print tokLoc
+        tokenSeqs = [(i, i+len(tokLoc)) for i in range(len(wordsInSentence)) if wordsInSentence[i:i+len(tokLoc)] == tokLoc]
+        #print tokenSeqs
+        for tokenSeq in tokenSeqs:
+            for tokenNo in range(tokenSeq[0], tokenSeq[1]):
+                sentence["tokens"][tokenNo]["ner"]  = "LOCATION"
 
 def getLocations(sentence):
     # note that a location can span multiple tokens
@@ -243,12 +266,24 @@ pattern2location2values = {}
 
 print str(len(jsonFiles)) + " files to process"
 
+# load the hardcoded names (if any):
+tokenizedLocationNames = []
+if len(sys.argv) > 3:
+    names = codecs.open(sys.argv[3], encoding='utf-8').readlines()
+    for name in names:
+        tokenizedLocationNames.append(unicode(name).split())
+print "Dictionary with hardcoded tokenized location names"
+print tokenizedLocationNames
+
 for fileCounter, jsonFileName in enumerate(jsonFiles):
     print "processing " + jsonFileName
-    with open(jsonFileName) as jsonFile:
+    with codecs.open(jsonFileName) as jsonFile:
         parsedSentences = json.loads(jsonFile.read())
     
     for sentence in parsedSentences:
+        # fix the ner tags
+        if len(tokenizedLocationNames)>0:
+            dictLocationMatching(sentence, tokenizedLocationNames)
         
         tokenIDs2number = getNumbers(sentence)
         tokenIDs2location = getLocations(sentence)

60c3b814faa86ec40677fe44df39aa4866603b3b
andreasvlachos <av308@cl.cam.ac.uk>
2014-11-11 20:00:33 +0000

    Fixed bug in turning the data to region2pattern2value, added a threshold on the weights to be used for prediction.

--- src/main/regressionWithMASE.py
+++ src/main/regressionWithMASE.py
@@ -15,19 +15,24 @@ class RegressionWithMASE(abstractPredictor.AbstractPredictor):
               
         self.region2feature2value = {}
         
+        self.predictionThreshold = None
+        
     def predict(self, property, region):
 
-        if region in self.region2feature2value:# and property =="/location/statistical_region/population":
-            # add the median just in case
+        if region in self.region2feature2value and property =="/location/statistical_region/population":
+            print "Predicting ", region
             prediction = 0.0
             weights = 0.0
             for feature, value in self.region2feature2value[region].items():
                 if feature in self.property2feature2weight[property]:
-                    # but subtract it from all the features
-                    weights += self.property2feature2weight[property][feature]
-                    prediction += (value - self.property2median[property])* self.property2feature2weight[property][feature]
+                    # set a threshold for a feature to be used
+                    if self.property2feature2weight[property][feature] > self.predictionThreshold:
+                        print "active feature ", feature, " value ", value , " weight ", self.property2feature2weight[property][feature]
+                         
+                        weights += self.property2feature2weight[property][feature]
+                        prediction += value* self.property2feature2weight[property][feature]
             if weights > 0: 
-                return prediction/weights + self.property2median[property]
+                return prediction/weights # + self.property2median[property]
             else:
                 print "No active features for region ", region.encode('utf-8'), " returning the median for the property"
                 return self.property2median[property]
@@ -81,6 +86,8 @@ class RegressionWithMASE(abstractPredictor.AbstractPredictor):
         simParam = float(params[1])
         # less than this won't make sense to calculate MASE 
         minOccurrences = 2
+        # this is used only for prediction
+        self.predictionThreshold = float(params[2])
 
         # change the text matrix from pattern2region2value to region2feature2value
         for pattern, region2value in textMatrix.items():
@@ -88,7 +95,7 @@ class RegressionWithMASE(abstractPredictor.AbstractPredictor):
                 for region, value in region2value.items():
                     if region not in self.region2feature2value:
                         self.region2feature2value[region] = {}
-                self.region2feature2value[region][pattern] = value         
+                    self.region2feature2value[region][pattern] = value         
 
         # for each property
         for property, trainRegion2value in trainMatrix.items():
@@ -101,71 +108,73 @@ class RegressionWithMASE(abstractPredictor.AbstractPredictor):
             # let's get the median for the property
             self.property2median[property] = numpy.median(trainRegion2value.values())
             print "median ", self.property2median[property]
-            #if property == "/location/statistical_region/population":
-            # let's filter out features we don't have a lot in the training data.
-            filteredTextMatrix = {}
-            for patternNo, (pattern, region2value) in enumerate(textMatrix.items()):
-                #print pattern, region2value
-                # if we have at least two values in common with the training data
-                regionsInCommon = set(region2value.keys()) & set(trainRegion2value.keys())
-                # also, avoid features that the same value everywhere.
-                if len(regionsInCommon) >= minOccurrences and (min(region2value.values()) < max(region2value.values())):# and "population" in pattern:
-                    filteredTextMatrix[pattern] = copy(region2value)
-                    pattern2column[pattern] = patternNo
-                    column2pattern.append(pattern)          
+            if property == "/location/statistical_region/population":
+                # let's filter out features we don't have a lot in the training data.
+                filteredTextMatrix = {}
+                for patternNo, (pattern, region2value) in enumerate(textMatrix.items()):
+                    #print pattern, region2value
+                    # if we have at least two values in common with the training data
+                    regionsInCommon = set(region2value.keys()) & set(trainRegion2value.keys())
+                    # also, avoid features that the same value everywhere.
+                    if len(regionsInCommon) >= minOccurrences and (min(region2value.values()) < max(region2value.values())):# and "population" in pattern:
+                        filteredTextMatrix[pattern] = copy(region2value)
+                        pattern2column[pattern] = patternNo
+                        column2pattern.append(pattern)          
                 
-            print "Text features after removing those with at least ", minOccurrences, " occurrences:", len(filteredTextMatrix)
-            #print column2pattern
-            #print filteredTextMatrix.keys()
+                print "Text features after removing those with at least ", minOccurrences, " occurrences:", len(filteredTextMatrix)
+                #print column2pattern
+                #print filteredTextMatrix.keys()
     
-            # So we will construct a system Aw = b, to solve argmin_x(sum(y-wx)^2)
-            # A is just XX^T or intuitively the covariance matrix among the features,
-            # b is Xy 
+                # So we will construct a system Aw = b, to solve argmin_x(sum(y-wx)^2)
+                # A is just XX^T or intuitively the covariance matrix among the features,
+                # b is Xy 
                 
-            # Given that A[i][j] = exp(param*symMAPE(a,1,2,), the diagonal is 1 since symmetric MAPE is 0
-            A = scipy.identity(len(filteredTextMatrix))
+                # Given that A[i][j] = exp(param*symMAPE(a,1,2,), the diagonal is 1 since symmetric MAPE is 0
+                A = scipy.identity(len(filteredTextMatrix))
                 
-            for i in range(len(filteredTextMatrix)): 
-                for j in range(i+1, len(filteredTextMatrix)):
-                    expSymMASE = 0
+                for i in range(len(filteredTextMatrix)): 
+                    for j in range(i+1, len(filteredTextMatrix)):
+                        expSymMASE = 0
                         
-                    feature1 = filteredTextMatrix[column2pattern[i]]
-                    feature2 = filteredTextMatrix[column2pattern[j]]
-                    # get the regions in common these features have
-                    regionsInCommon = set(feature1.keys()) & set(feature2.keys())
-                    # if they have enough in common
-                    if len(regionsInCommon) > minOccurrences:
-                        symMASE = self.symmetrizedMASE(feature1,feature2)
-                        scalingFactor = float(scalingParam)/(scalingParam + len(regionsInCommon))
+                        feature1 = filteredTextMatrix[column2pattern[i]]
+                        feature2 = filteredTextMatrix[column2pattern[j]]
+                        # get the regions in common these features have
+                        regionsInCommon = set(feature1.keys()) & set(feature2.keys())
+                        # if they have enough in common
+                        if len(regionsInCommon) > minOccurrences:
+                            symMASE = self.symmetrizedMASE(feature1,feature2)
+                            scalingFactor = float(scalingParam)/(scalingParam + len(regionsInCommon))
         
-                        expSymMASE = numpy.exp(-simParam*scalingFactor*symMASE)
-                    A[i][j] = expSymMASE
-                    A[j][i] = expSymMASE 
+                            expSymMASE = numpy.exp(-simParam*scalingFactor*symMASE)
+                        A[i][j] = expSymMASE
+                        A[j][i] = expSymMASE 
                 
-            b = scipy.zeros(len(filteredTextMatrix))
+                b = scipy.zeros(len(filteredTextMatrix))
                 
-            for i in range(len(filteredTextMatrix)):
-                # a feature that doesn'have enoguh in common with the property we are trying to predict
-                # must have been filtered out already
-                regionsInCommon = set(filteredTextMatrix[column2pattern[i]].keys()) & set(trainRegion2value.keys())
-                scalingFactor = float(scalingParam)/(scalingParam + len(regionsInCommon))
-                symMASE = self.symmetrizedMASE(filteredTextMatrix[column2pattern[i]], trainRegion2value)
-                b[i] = numpy.exp(-simParam*scalingFactor*symMASE)
-            print "constructed the system of equations"
+                for i in range(len(filteredTextMatrix)):
+                    # a feature that doesn'have enough in common with the property we are trying to predict
+                    # must have been filtered out already
+                    regionsInCommon = set(filteredTextMatrix[column2pattern[i]].keys()) & set(trainRegion2value.keys())
+                    scalingFactor = float(scalingParam)/(scalingParam + len(regionsInCommon))
+                    symMASE = self.symmetrizedMASE(filteredTextMatrix[column2pattern[i]], trainRegion2value)
+                    b[i] = numpy.exp(-simParam*scalingFactor*symMASE)
+                print "constructed the system of equations"
                              
                 # solve for them using least squares (the exact solver is likely to fail, we have many more features usually)
                 # This is not necessarily the best option, as the objective is now non-convex?
-            weights, res = scipy.optimize.nnls(A, b)
-            print weights
-            print res
-            self.property2feature2weight[property] = {}
-            for i, weight in enumerate(weights):
-                #print i, weight, column2pattern[i]                    
-                self.property2feature2weight[property][column2pattern[i]] = weight
-                    
-            sortedPatterns = sorted(self.property2feature2weight[property].items(), key=operator.itemgetter(1), reverse=True)
-            print "top-100 patterns found"
-            print sortedPatterns[:100]
+                weights, res = scipy.optimize.nnls(A, b)
+                #print weights
+                #print res
+                self.property2feature2weight[property] = {}
+                for i, weight in enumerate(weights):
+                    #print i, weight, column2pattern[i]               
+                    if weight> 0.0:
+                        self.property2feature2weight[property][column2pattern[i]] = weight
+                print len(self.property2feature2weight[property]), " active features"    
+                sortedPatterns = sorted(self.property2feature2weight[property].items(), key=operator.itemgetter(1), reverse=True)
+                
+                print "top100 patterns found"
+                print sortedPatterns[:100]
                       
 if __name__ == "__main__":
     
@@ -181,5 +190,5 @@ if __name__ == "__main__":
     
     # if the first param is False, the other are ignored and no neighborhood is active.
     # thus it is the same as regression 0, True
-    bestParams = regressionWithMASE.crossValidate(trainMatrix, textMatrix, 4, [[1,1], [2,1], [0.5,1]])
+    bestParams = regressionWithMASE.crossValidate(trainMatrix, textMatrix, 4, [[0.25,1,0.9], [0.25,1, 0.8],[0.25,1, 0.7]])
     regressionWithMASE.runEval(trainMatrix, textMatrix, testMatrix, bestParams)

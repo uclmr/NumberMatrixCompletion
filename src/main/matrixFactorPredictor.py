8c6cf6c4f57de3c3cbc177df724f5312b9bbd60f
Andreas Vlachos <av308@cl.cam.ac.uk>
2014-11-14 01:46:20 +0000

    Added an option for scaling the errors similar to the MASE scaling, but didn't help.

--- src/main/matrixFactorPredictor.py
+++ src/main/matrixFactorPredictor.py
@@ -12,24 +12,42 @@ class MatrixFactorPredictor(abstractPredictor.AbstractPredictor):
         return numpy.dot(self.propertyOrPattern2vector[property], self.region2Vector[region])
     
     # parameters are: dimensions of vectors, learning rate, reg_parameter, iterations
-    def train(self, trainMatrix, textMatrix, params=[10,0.1,1, 5000]):
+    def train(self, trainMatrix, textMatrix, params=[10,0.1,1, 5000, True]):
         
-        dims, learningRate, regParam, iterations = params
+        dims, learningRate, regParam, iterations, scale = params
         # possibly filter to avoid features with low counts
         
         # initialize the low dim representations
         # first the properties
+        if scale:
+            propertyOrPattern2scalingFactor = {}
         for property, region2value in trainMatrix.items():
             self.propertyOrPattern2vector[property] = numpy.random.rand(dims)
+            if scale:
+                # if we are scaling, calculate the scaling factor, i.e. the average squared median error
+                median = numpy.median(region2value.values())
+                squaredMedianErrors = []
+                for val in region2value.values():
+                    squaredMedianErrors.append(numpy.square(val - median))
+                propertyOrPattern2scalingFactor[property] = numpy.mean(squaredMedianErrors)
+                
             for region in region2value.keys():
                 if region not in self.region2Vector:
-                    self.region2Vector[region] = numpy.random.rand(dims)
+                    self.region2Vector[region] =  numpy.random.rand(dims)
 
         for pattern, region2value in textMatrix.items():
             self.propertyOrPattern2vector[pattern] = numpy.random.rand(dims)
+            if scale:
+                # if we are scaling, calculate the scaling factor, i.e. the average squared median error
+                median = numpy.median(region2value.values())
+                squaredMedianErrors = []
+                for val in region2value.values():
+                    squaredMedianErrors.append(numpy.square(val - median))
+                propertyOrPattern2scalingFactor[pattern] = numpy.mean(squaredMedianErrors)
+            
             for region in region2value.keys():
                 if region not in self.region2Vector:
-                    self.region2Vector[region] = numpy.random.rand(dims)                    
+                    self.region2Vector[region] =  numpy.random.rand(dims)                    
                      
         
         # let's go!
@@ -45,7 +63,11 @@ class MatrixFactorPredictor(abstractPredictor.AbstractPredictor):
                 for region, value in region2value.items():
                     # reconstruction error
                     eij = value - numpy.dot(self.propertyOrPattern2vector[pp],self.region2Vector[region])
-                    #print pp, " ", region, " error=",eij
+                    # if we are scaling
+                    if scale:
+                        print "scaling factor for ", pp ,":", propertyOrPattern2scalingFactor[pp]
+                        eij = eij/propertyOrPattern2scalingFactor[pp]
+                    print pp, " ", region, " error=",eij
                     for k in xrange(dims):
                         self.propertyOrPattern2vector[pp][k] += learningRate * (2 * eij * self.region2Vector[region][k] - regParam * self.propertyOrPattern2vector[pp][k])
                         self.region2Vector[region][k] += learningRate * (2 * eij * self.propertyOrPattern2vector[pp][k] - regParam * self.region2Vector[region][k])        
@@ -53,13 +75,16 @@ class MatrixFactorPredictor(abstractPredictor.AbstractPredictor):
         
             # let's calculate the squared reconstruction error
             # maybe look only at the training data?
-            re = 0
+            squaredErrors = []
             for property, region2value in trainMatrix.items():
                 for region, value in region2value.items():
                     pred = self.predict(property, region)
-                    re += numpy.square(pred - value)
+                    if scale:
+                        squaredErrors.append(numpy.square(pred - value)/propertyOrPattern2scalingFactor[pp])
+                    else:
+                        squaredErrors.append(numpy.square(pred - value))
              
-            print "Iteration ", iter, " reconstruction RMSE on trainMatrix=", numpy.sqrt(re)  
+            print "Iteration ", iter, " reconstruction average (possibly scaled) squared errors on trainMatrix=", numpy.mean(squaredErrors) 
 if __name__ == "__main__":
     
     import sys
@@ -74,5 +99,5 @@ if __name__ == "__main__":
     textMatrix = abstractPredictor.AbstractPredictor.loadMatrix(sys.argv[2])
     testMatrix = abstractPredictor.AbstractPredictor.loadMatrix(sys.argv[3])
 
-    bestParams = MatrixFactorPredictor.crossValidate(trainMatrix, textMatrix, 4, [[10, 0.000000000000001,0.0001, 5000]])
+    bestParams = MatrixFactorPredictor.crossValidate(trainMatrix, textMatrix, 4, [[10, 0.000001,0.0001, 5000, True]])
     #MatrixFactorPredictor.runEval(trainMatrix, textMatrix, testMatrix, bestParams)
